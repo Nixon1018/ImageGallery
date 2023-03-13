5e4d24a9facd0169b47cbdabf54f2bce9b846ab1
Nixon1018 <123439949+Nixon1018@users.noreply.github.com>
2023-03-13 10:11:40 +0530

    Add files via upload

--- ImageGallery.js
+++ ImageGallery.js
@@ -1,21 +1,36 @@
-const lightbox = document.createElement('div')
-lightbox.id='lightbox'
-document.body.appendChild(lightbox)
 
-const images = document.querySelectorAll('img')
-images.forEach(image => {
-  image.addEventListener('click', e => {
-    lightbox.classList.add('active')
-    const img = document.createElement('img')
-    img.src = image.src
-    while (lightbox.firstChild) {
-      lightbox.removeChild(lightbox.firstChild)
-    }
-    lightbox.appendChild(img)
-  })
-})
+const arr = [{"id":0,"image":"images/1.jpg"},
+              {"id":1,"image":"images/2.jpg"},
+              {"id":2,"image":"images/3.jpg"},
+              {"id":3,"image":"images/4.jpg"},
+              {"id":4,"image":"images/5.jpg"},
+              {"id":5,"image":"images/6.jpg"},
+              {"id":6,"image":"images/7.jpg"},
+              {"id":7,"image":"images/8.jpg"},
+              {"id":8,"image":"images/9.jpg"}]
+let image = ""
 
-lightbox.addEventListener('click', e => {
-  if (e.target !== e.currentTarget) return
-  lightbox.classList.remove('active')
-})
+for(let img of arr){
+  image+="<img src='"+img.image+"' onclick='overlay("+img.id+")'>"
+}
+document.getElementById("gallery").innerHTML = image
+
+
+const closebox = () =>{
+  console.log("hello")
+  let element = document.getElementById("overlay")
+  element.style.display = "none"
+}
+
+
+const overlay = (id) =>{
+  if(id<0){
+    id = arr.length-1
+  }
+  else if(id == arr.length){
+    id = 0
+  }
+  let element = document.getElementById("overlay")
+  element.innerHTML = "<button class='close' onclick='closebox()'>X</button><div><button onclick='overlay("+(id-1)+")'>⟨</button><img src='"+arr[id].image+"'> <button onclick='overlay("+(id+1)+")'>⟩</button></div>"
+  element.style.display = "flex"
+}
